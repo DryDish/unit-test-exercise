9db22682d7d99aafb9c8814fc05e1eccd79bc831
DryDish <57399961+DryDish@users.noreply.github.com>
2022-03-09 13:33:21 +0100

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use crate::length::{Length, Unit};
 
 
 fn main() {
-    let unit = Length::new(123.13, Unit::Metric);
-    println!("{}", unit);
-    println!("{}", unit.convert());
+    length_run();
+}
+
+fn length_run() {
+    let metric_length = Length::new(123.13, Unit::Metric);
+    let imperial_length = Length::new(12.1, Unit::Imperial);
+    println!("{}", metric_length);
+    println!("Converted to Imperial: {}", metric_length.convert());
+    println!("{}", imperial_length);
+    println!("Converted to Metric: {}", imperial_length.convert());
 }
