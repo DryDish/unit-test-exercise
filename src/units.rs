3a7692f619b46f52f62c76264a1eb44a654683a9
DryDish <57399961+DryDish@users.noreply.github.com>
2022-03-09 15:00:49 +0100

    add Temperature to units

--- src/units.rs
+++ src/units.rs
@@ -15,3 +15,21 @@ impl std::fmt::Display for Unit {
         }
     }
 }
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Temperature {
+    Celsius,
+    Fahrenheit,
+    Kelvin
+}
+
+impl std::fmt::Display for Temperature {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Temperature::Celsius => { write!(f, "Celsius")},
+            Temperature::Fahrenheit => { write!(f, "Fahrenheit")},
+            Temperature::Kelvin => { write!(f, "Kelvin")}
+        }
+    }
+}
